c930e8bc4a02af4eba0da413030522f3795d15f4
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-06-01 17:56:53 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {ThemeContext} from "./contents/ThemeContext.jsx"
+import { useContext } from "react";
 
-function App() {
+ function App() {
+   const {status,handleStatus} = useContext(ThemeContext);
   return (
+    // App-Light  App-header
     <div className="App">
-      <header className="App-header">
+      <header className={status ? "App-Light" : "App-header"}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{padding:" 15px 55px 15px 55px",fontSize:"22px", borderRadius:"8px"}} onClick={handleStatus}>{status ? "Dark" : "Light"}</button>
+        <h1>{status ? "Switch To Dark Mode" : "Switch To Light Mode"}</h1>
       </header>
     </div>
   );
 }
+
 
 export default App;
